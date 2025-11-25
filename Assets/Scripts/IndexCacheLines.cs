5e9ba28090e5ab04439d77f4857cc32640abdb46
toveandersson <toveama@gmail.com>
2025-11-26 00:01:06 +0100

    build

--- Assets/Scripts/IndexCacheLines.cs
+++ Assets/Scripts/IndexCacheLines.cs
@@ -1,9 +1,3 @@
-using static Logic;
-using static System.Net.WebRequestMethods;
-using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
-
-using Unity.VisualScripting;
-
-//< meta http - equiv = "Cache-Control" content = "no-cache, no-store, must-revalidate" />
-//    < meta http - equiv = "Pragma" content = "no-cache" />
-//    < meta http - equiv = "Expires" content = "0" />
+//<meta http-equiv = "Cache-Control" content = "no-cache, no-store, must-revalidate" />
+//<meta http-equiv = "Pragma" content = "no-cache" />
+//<meta http-equiv = "Expires" content = "0" />
